d558226c011cf83e95516215f51fedd0d3cf289b
monugya-07 <monugyaborchetia1@gmail.com>
2026-02-12 18:06:45 +0530

    added necessary templates

--- Django_P_03_Ecommerce/YBalpha/alpha/views.py
+++ Django_P_03_Ecommerce/YBalpha/alpha/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def home(request):
+    # return HttpResponse("Hello, This is YBalpha home page")
+    return render(request, 'website/main.html')
 
 def about(request):
     return render(request, 'alpha/about.html')
